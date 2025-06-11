39960e58cd04e34f0f700f79f6bf54e6c7af7dd3
Valery Hoyos <73263344+valery-22@users.noreply.github.com>
2025-06-11 13:52:38 -0500

    Add files via upload

---
+++ adding_loop.py
@@ -0,0 +1,8 @@
+# List of fruits to be used in fruit salad
+fruits = ['apple', 'banana', 'kiwi', 'mango']
+
+# TODO: Loop over the list of fruits and add them to the fruit salad with a print statement
+for fruit in fruits:
+    
+    # TODO: Print a statement for adding each fruit to the fruit salad.
+    print(f"Adding {fruit}in the sald.")
